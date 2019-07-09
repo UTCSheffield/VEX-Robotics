518b040a42682ed0d0713811ebd2d4726ed655e6
16mossm <50373533+16mossm@users.noreply.github.com>
2019-07-09 16:29:01 +0100

    Update epicsupertest12345678910ofmath.py
    passsses current tests because i am a epic lad
    
    1234567890

--- Tower Takeover/Untitled/epicsupertest12345678910ofmath.py
+++ Tower Takeover/Untitled/epicsupertest12345678910ofmath.py
@@ -1,10 +1,10 @@
 import math
-print(math.degrees(math.atan(1)))
+
 def mmm(x1,y1,potato1,x2,y2,potato2):
     """return turn values and distance
 
     >>> mmm(0,0,0, 3,4,180)
-    (36.870, 5.0, 143.130)
+    (36.87, 5.0, 143.13)
 
     >>> mmm(0,0,0, 0,1,0)
     (0, 1.0, 0)
@@ -17,7 +17,8 @@ def mmm(x1,y1,potato1,x2,y2,potato2):
     if(x3 == 0):
         turn1 = 0
     else:
-        turn1 =  math.degrees(math.atan(y3/x3))
+        turn1 =  round(90 - math.degrees(math.atan(y3/x3)),3)
+        
     dist = math.sqrt((x3**2)+(y3**2))
     turn2 = potato2 - (potato1 + turn1)
     #print(turn1)
