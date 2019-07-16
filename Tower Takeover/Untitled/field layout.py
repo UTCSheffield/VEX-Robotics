692763089ee2e6f8ffd2a8c64874e41d8090854e
billypele2005 <52710073+billypele2005@users.noreply.github.com>
2019-07-16 16:25:53 +0100

    Layout of the blocks

---
+++ Tower Takeover/Untitled/field layout.py
@@ -0,0 +1,58 @@
+
+blocks_blue = [
+{num:1, colour:Green, x=1, y:2, anchor=BottomLeft},
+{num:2, colour:orange, attached:1, direction:Out},
+{num:3, colour:orange, attached:2, direction:Out},
+{num:4, colour:purple, attached:3,direction:Out},
+
+{num:5, colour:orange, x=2, y:2, anchor=BottomLeft},
+{num:6, colour:green, attached: 5,direction;Up},
+{num:7, colour:orange, attached: 5, direction:Out},
+{num:8, colour:purple, attached: 7, direction:Out},
+
+{num:9, colour:orange, x=3, y:2, anchor=BottomLeft},
+{num:10, colour:orange, attached: 9, direction:Up},
+{num:11, colour:green, attached: 10,direction;Up},
+{num:12, colour:purple, attached: 9, direction:Out},
+
+
+
+{num:13, colour:purple, x=4, y:2, anchor=BottomLeft},
+{num:14, colour:orange, attached: 13, direction:Up},
+{num:15, colour:orange, attached: 14,direction;Up},
+{num:16, colour:purple, attached: 15, direction:Up},
+
+
+
+{num:17, colour:green, x=4, y:1, anchor=BottomLeft},
+
+
+
+{num:18, colour:green, x=5, y:1, anchor=BottomLeft},
+
+
+
+{num:19, colour:purple, x=6, y:3, anchor=MiddleRight},
+
+
+
+{tower:1, colour:blue, x=0,y=1.5},
+{tower:2, colour:black, x=3,y=1},
+{tower:3, colour:black, x=1.5,y=3},
+{tower:4, colour:black, x=3,y=3},
+{tower:5, colour:black, x=4.5,y=3},
+
+
+
+{num:9, colour:orange, x=3, y:2, anchor=BottomLeft},
+{num:10, colour:orange, attached: 9, direction:Up},
+{num:11, colour:green, attached: 10,direction;Up},
+{num:12, colour:purple, attached: 9, direction:Out},
+
+
+
+
+
+
+
+    ]
