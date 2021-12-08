5b4b87b229c2b6bff4f876e60748bf8dd210d0f8
Matthew Moss <16mossm@utcsheffield.org.uk>
2021-12-08 16:26:57 +0000

    basic driving through  config

---
+++ Tipping Point/vainglorious_boodongle/DriveCode/src/main.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\16mossm                                          */
+/*    Created:      Wed Dec 08 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 2            
+// MotorGroupArm        motor_group   11, 12          
+// MotorGroupFork       motor_group   5, 6            
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
