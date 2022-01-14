c4f18c050afbc1b6ce7dd8c25ed02c4547225539
Sean Outram <81807361+devramsean0@users.noreply.github.com>
2022-01-14 14:51:22 +0000

    Forklift should work now lol

--- Tipping Point/Vteepee/TankTeepee/src/main.cpp
+++ Tipping Point/Vteepee/TankTeepee/src/main.cpp
@@ -5,71 +5,19 @@
 // Controller1          controller                    
 // forklift             motor_group   3, 4            
 // ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 2, A         
-// Controller1          controller                    
-// forklift             motor_group   3, 4            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 2, A         
-// Controller1          controller                    
-// forklift             motor_group   3, 4            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 2, A         
-// Controller1          controller                    
-// forklift             motor_group   3, 4            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 2, A         
-// Controller1          controller                    
-// forklift             motor_group   3, 4            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 2, A         
-// Controller1          controller                    
-// forklift             motor_group   3, 4            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Wed Sep 25 2019                                           */
-/*    Description:  Clawbot (Drivetrain) - Template                           */
-/*                                                                            */
-/*    Name:                                                                   */
-/*    Date                                                                    */
-/*    Class:                                                                  */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 2, A         
-// ClawMotor            motor         3               
-// ArmMotor             motor         8               
-// Controller1          controller                    
-// ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
-int forklift_positions[3] = {0, 30, 60};
+int forklift_positions[3] = {90, 60, 0};
 int forklift_position = 0;
-int forklift_motor_degrees(int forklift_degrees) {
+int forklift_positions_length = sizeof(forklift_positions)/sizeof(forklift_positions[0]);
+
+int forklift_motor_degrees(int pos) {
+  int forklift_degrees = forklift_positions[pos % 3];
   return 1 * forklift_degrees;
 }
+
 // autonomous functions!s
 void autonomousleft() {
   Drivetrain.turn(left);
@@ -92,6 +40,7 @@ int main() {
   forklift.resetPosition();
   forklift.setStopping(hold);
   bool forkliftpressed = false;
+  
   //orklift.spinToPosition(forklift_motor_degrees(forklift_positions[forklift_position]),deg);
   while(true) {
     // Spin both motors in the forward direction.
@@ -99,17 +48,42 @@ int main() {
     //RightMotor.spin(forward);
 
     // FORKLIFT
-    if (Controller1.ButtonR2.pressing() && forkliftpressed==false) {
-      forkliftpressed = true;
-      forklift_position ++ ;
-      Brain.Screen.print("\nforklift to pos ");
-      Brain.Screen.print(forklift_position);
-      int motorRotation = forklift_motor_degrees(forklift_positions[forklift_position]);
-      Brain.Screen.print("forklift rotation ");
-      Brain.Screen.print(motorRotation);
-      
-      forklift.spinToPosition(motorRotation ,deg);
-      
+    
+    if (Controller1.ButtonR1.pressing() || Controller1.ButtonR2.pressing()) {
+      if (forkliftpressed==false && Controller1.ButtonR2.pressing() ) {
+        forkliftpressed = true;
+        forklift_position ++ ;
+        if(forklift_position < 0){
+          forklift_position = 0;
+        }
+        if(forklift_position >= forklift_positions_length) {
+          forklift_position = forklift_positions_length -1;
+        }
+        Brain.Screen.print("ButtonR2 pos %d", forklift_position);     
+        int motorRotation = forklift_motor_degrees(forklift_position);
+        Brain.Screen.print("motorRotation %d", motorRotation);
+        Brain.Screen.newLine(); 
+        
+        forklift.spinToPosition(motorRotation ,deg);
+        
+      }
+      else if (forkliftpressed==false && Controller1.ButtonR1.pressing()) {
+        forkliftpressed = true;
+        forklift_position -- ;
+        if(forklift_position < 0){
+          forklift_position = 0;
+        }
+        if(forklift_position >= forklift_positions_length) {
+          forklift_position = forklift_positions_length -1;
+        }
+        Brain.Screen.print("ButtonR1 pos %d", forklift_position);     
+        int motorRotation = forklift_motor_degrees(forklift_position);
+        Brain.Screen.print(" motorRotation %d", motorRotation);
+        Brain.Screen.newLine(); 
+
+        forklift.spinToPosition(motorRotation ,deg);
+        
+      }
     }
     else
     {
